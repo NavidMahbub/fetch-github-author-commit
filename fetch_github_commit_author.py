56e6c5ace40423cd8c36208e2912bf02d1c576ba
NavidMahbub <navidmahbub27@gmail.com>
2025-02-06 12:43:07 +0600

    python script to fetch commits and its author

---
+++ fetch_github_commit_author.py
@@ -0,0 +1,15 @@
+import requests
+
+REPO = 'NavidMahbub/fetch-github-author-commit'
+URL = f'https://api.github.com/repos/{REPO}/commits'
+
+response = requests.get(URL)
+
+if response.status_code == 200:
+    commits = response.json()
+    for commit in commits:
+        commit_message = commit['commit']['message']
+        author_name = commit['commit']['author']['name']
+        print(f'Author: {author_name}, Commit Message: {commit_message}')
+else:
+    print(f'Failed to fetch commits: {response.status_code}')
