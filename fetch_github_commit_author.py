fa10e04803d9f93759a1d53cd81f4b5c7b6d0eaa
NavidMahbub <navidmahbub27@gmail.com>
2025-02-06 13:09:22 +0600

    better repsentation

--- fetch_github_commit_author.py
+++ fetch_github_commit_author.py
@@ -5,6 +5,10 @@ URL = f'https://api.github.com/repos/{REPO}/commits'
 
 response = requests.get(URL)
 
+print('\n========================================================')
+print('===================== Author & Commits =================')
+print('========================================================')
+
 if response.status_code == 200:
     commits = response.json()
     for commit in commits:
@@ -13,3 +17,6 @@ if response.status_code == 200:
         print(f'Author: {author_name}, Commit Message: {commit_message}')
 else:
     print(f'Failed to fetch commits: {response.status_code}')
+
+print('\n========================================================')
+print('========================================================')
